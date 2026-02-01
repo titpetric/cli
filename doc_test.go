09e1848968c913e3e553d427f95fb3a78b9758c5
Tit Petric <black@titpetric.com>
2026-02-01 10:23:03 +0100

    Add example of use for godoc

---
+++ doc_test.go
@@ -0,0 +1,41 @@
+package cli_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/titpetric/cli"
+)
+
+func ExampleApp() {
+	app := cli.NewApp("mig")
+
+	app.AddCommand("version", "Print version information", func() *cli.Command {
+		var verbose bool
+
+		return &cli.Command{
+			Name:  "version",
+			Title: "Print version information",
+			Bind: func(fs *cli.FlagSet) {
+				cli.BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+			},
+			Run: func(ctx context.Context, args []string) error {
+				if verbose {
+					fmt.Println("mig version 1.2.3 (commit abcdef)")
+				} else {
+					fmt.Println("mig version 1.2.3")
+				}
+				return nil
+			},
+		}
+	})
+
+	// In a real program you would call:
+	//     _ = app.Run()
+	//
+	// For examples/tests, invoke RunWithArgs directly.
+	_ = app.RunWithArgs([]string{"version"})
+
+	// Output:
+	// mig version 1.2.3
+}
